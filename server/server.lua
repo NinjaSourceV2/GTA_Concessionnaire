23733aadf7870e88095c474b002bf1bd533f6d6e
Ninja <samir932205@gmail.com>
2021-04-27 00:43:09 +0200

    udpate paiement

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,5 @@
 --> Version de la Resource : 
-local LatestVersion = ''; CurrentVersion = '1.8'
+local LatestVersion = ''; CurrentVersion = '1.9'
 PerformHttpRequest('https://raw.githubusercontent.com/NinjaSourceV2/GTA_Concessionnaire/master/VERSION', function(Error, NewestVersion, Header)
     LatestVersion = NewestVersion
     if CurrentVersion ~= NewestVersion then
@@ -11,21 +11,23 @@ RegisterServerEvent("GTA_Concess:PayerVehicule")
 AddEventHandler("GTA_Concess:PayerVehicule", function(prix, index, id, veh, newVehicleNom, model, plate, primarycolor, secondarycolor, pearlescentcolor, wheelcolor)
     local source = source
     TriggerEvent('GTA:GetInfoJoueurs', source, function(data)
-        local identifier = GetPlayerIdentifiers(source)[1]
-        local cash = data.argent_propre
-        local nom = data.nom
-        local prenom = data.prenom
-        local proprietaire = (nom .. " " ..prenom) 
+        TriggerEvent('GTA:GetUserQtyItem', source, "Argent-Propre", function(argentPropreQty)
+            local identifier = GetPlayerIdentifiers(source)[1]
+            local cash = argentPropreQty
+            local nom = data.nom
+            local prenom = data.prenom
+            local proprietaire = (nom .. " " ..prenom) 
 
-        if (tonumber(cash) >= prix) then
-            TriggerClientEvent("GTA_Concess:PaiementEffectuer", source, index, id, veh, model, plate, primarycolor, secondarycolor, pearlescentcolor, wheelcolor)
-            TriggerEvent('GTA:RetirerArgentPropre', source, tonumber(prix))
-            local value = {identifier, newVehicleNom, model, plate, "Sortit", primarycolor, secondarycolor, pearlescentcolor, wheelcolor, proprietaire, prix}
+            if (tonumber(cash) >= prix) then
+                TriggerClientEvent("GTA_Concess:PaiementEffectuer", source, index, id, veh, model, plate, primarycolor, secondarycolor, pearlescentcolor, wheelcolor)
+                TriggerEvent('GTA:RetirerArgentPropre', source, tonumber(prix))
+                local value = {identifier, newVehicleNom, model, plate, "Sortit", primarycolor, secondarycolor, pearlescentcolor, wheelcolor, proprietaire, prix}
 
-            MySQL.Sync.execute('INSERT INTO gta_joueurs_vehicle (`identifier`, `vehicle_name`, `vehicle_model`, `vehicle_plate`, `vehicle_state`, `vehicle_colorprimary`, `vehicle_colorsecondary`, `vehicle_pearlescentcolor`, `vehicle_wheelcolor`, `proprietaire`, `prix`) VALUES ?', { { value } })
-			TriggerClientEvent('nMenuNotif:showNotification', source, "~g~Paiement accepter~w~.")
-		else
-			TriggerClientEvent('nMenuNotif:showNotification', source, "~r~Tu n'as pas suffisamment d'argent !")
-		end
+                MySQL.Sync.execute('INSERT INTO gta_joueurs_vehicle (`identifier`, `vehicle_name`, `vehicle_model`, `vehicle_plate`, `vehicle_state`, `vehicle_colorprimary`, `vehicle_colorsecondary`, `vehicle_pearlescentcolor`, `vehicle_wheelcolor`, `proprietaire`, `prix`) VALUES ?', { { value } })
+                TriggerClientEvent('nMenuNotif:showNotification', source, "~g~Paiement accepter~w~.")
+            else
+                TriggerClientEvent('nMenuNotif:showNotification', source, "~r~Tu n'as pas suffisamment d'argent !")
+            end
+        end)
     end)
 end)
